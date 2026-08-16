103a2f7247a46daa3a1974a3e9e3527116530538
pxlman <agxgv7573@gmail.com>
2026-08-17 00:26:31 +0300

    feat: stop logging body and response to isolate production's logs from showing insecure stuff

--- server/src/logger/logger.middleware.ts
+++ server/src/logger/logger.middleware.ts
@@ -5,18 +5,18 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const { method, originalUrl, body, query, params } = req;
     console.log(`[${method}] ${originalUrl}`);
-    if (Object.keys(body).length > 0) {
-      console.log('Body:', body);
-    }
+    // if (Object.keys(body).length > 0) {
+    //   console.log('Body:', body);
+    // }
     if (Object.keys(query).length > 0) {
       console.log('Query:', query);
     }
     if (Object.keys(params).length > 1) {
       console.log('Params:', params);
     }
-    if (Object.keys(res).length > 0) {
-      console.log('Response:', res);
-    }
+    // if (Object.keys(res).length > 0) {
+    //   console.log('Response:', res);
+    // }
     next();
   }
 }
