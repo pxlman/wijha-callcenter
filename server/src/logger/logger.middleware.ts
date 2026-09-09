e41d475f4eda0a34994795036cd3499979b6f93f
pxlman <agxgv7573@gmail.com>
2026-09-09 14:22:15 +0300

    fix: clean up logger middleware by removing commented code

--- server/src/logger/logger.middleware.ts
+++ server/src/logger/logger.middleware.ts
@@ -5,21 +5,20 @@ import type { Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
     const { method, originalUrl, body, query, params } = req;
-    const start = Date.now();
 
     console.log(`[${method}] ${originalUrl}`);
-    // if (Object.keys(body).length > 0) {
-    //   console.log('Body:', body);
-    // }
+
+    if(process.env.DEBUG === '1') {
+      if (Object.keys(body).length > 0) {
+        console.log('Body:', body);
+      }
+    }
     if (Object.keys(query).length > 0) {
       console.log('Query:', query);
     }
     if (params && Object.keys(params).length > 1) {
       console.log('Params:', params);
     }
-    // if (Object.keys(res).length > 0) {
-    //   console.log('Response:', res);
-    // }
     next();
   }
 }
